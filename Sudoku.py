74bc81edf0cdebb744d4374750c3128ee3fa10db
DanielJ <d.jimenez.0410@gmail.com>
2025-02-10 14:13:48 -0500

    Update Sudoku.py
    Finally found the time to revisit this project proper and do things I wanted to do.
    
    - Grid object
    - Cleaner code (for the most part)
    - Less redundancy (to some parts)
    - Better comments
    
    Coming in the future: a gui

--- Sudoku.py
+++ Sudoku.py
@@ -1,307 +1,247 @@
-#python script to solve sudoku puzzles
-#written by DanJ555
+class Grid:
 
-from random import choice
-from copy import copy, deepcopy
+    def __init__(self, puzzle: list[int] = None) -> None:
+        self.values_stream: list[Cell] = [Cell() for _ in range(81)]
+        self.rows: list[list[Cell]] = [[] for _ in range(9)]
+        self.columns: list[list[Cell]] = [[] for _ in range(9)]
+        self.boxes: list[list[Cell]] = [[] for _ in range(9)]
+        self.__establish_sublists()
+        self.printable: bool = False
+        if puzzle is not None:
+            self.set_values(puzzle)
 
-class cell:
+    def __establish_sublists(self) -> None:
+        row_index = -1
+        box_index = -1
+        for index, cell in enumerate(self.values_stream):
+            # establish columns
+            column_index = index % 9
+            self.columns[column_index].append(cell)
+            cell.parent_column = self.columns[column_index]
+            # establish rows
+            if column_index == 0:
+                row_index += 1
+            self.rows[row_index].append(cell)
+            cell.parent_row = self.rows[row_index]
+            # establish 3x3 boxes
+            if column_index % 3 == 0:
+                box_index += 1
+            self.boxes[box_index].append(cell)
+            cell.parent_box = self.boxes[box_index]
+            if column_index == 8 and index % 27 != 26:
+                box_index -= 3
+            cell.parent_grid = self
 
-    #class will initialize it's list of possible values if not given a value 1-9
-    def __init__(self,v):
-        self.value = v
-        if v == 0:
-            self.PossibleVals = [1,2,3,4,5,6,7,8,9]
-        else:
-            self.PossibleVals = []
-        self.Parents = [None,None,None] #Box, Row, Column
+    def __str__(self) -> str:
+        cumulative = f"{"_" * 19}\n"
+        for row in self.rows:
+            addition = "|"
+            for cell in row:
+                if cell.value == 0:
+                    addition += " |"
+                else:
+                    addition += str(f"{cell}|")
+            cumulative += f"{addition}\n"
+        cumulative += "¯" * 19
+        return cumulative
 
-    def __str__(self):
+    def set_values(self, puzzle: list[int]) -> None:
+        for index, value in enumerate(puzzle):
+            self.values_stream[index].assign_value(value)
+        self.printable = True
+        print(self)
+
+    def reset(self) -> None:
+        self.printable = False
+        for cell in self.values_stream:
+            cell.reset()
+        self.printable = True
+        print(self)
+
+    def print_possibles_by_box(self) -> None:
+        for box in self.boxes:
+            print(f"{"_" * 55}\nNew box output")
+            for cell in box:
+                print(f"{cell.__repr__()}")
+
+    def is_solved(self) -> bool:
+        for groups in (self.rows, self.columns, self.boxes):
+            for group in groups:
+                if sum(group) != 45:
+                    return False
+        print("Puzzle has been solved.")
+        return True
+
+    def solve(self) -> None:
+        # I just want this to work. I'm not too concerned about efficiency right now.
+        # Hidden Singles search
+        for index, groups in enumerate((self.rows, self.columns, self.boxes)):
+            for group in groups:
+                occurrences: list[list[Cell]] = []
+                for n in range(9):
+                    occurrences.append([])
+                    for cell in group:
+                        if (n + 1) in cell.possible_values:
+                            occurrences[n].append(cell)
+                for i, occurrence in enumerate(occurrences):
+                    occurrence_amount: int = len(occurrence)
+                    if occurrence_amount == 1:
+                        occurrence[0].assign_value(i + 1)
+        # Naked pairs
+        for box in self.boxes:
+            for cell in box:
+                if not cell.possible_values:
+                    pass
+                else:
+                    same_possibles: list[Cell] = []
+                    for sibling_cell in box:
+                        if cell.possible_values == sibling_cell.possible_values:
+                            same_possibles.append(sibling_cell)
+                    possibles = same_possibles[0].possible_values
+                    if len(same_possibles) == 2:
+                        if len(possibles) == 2:
+                            for cell_ii in box:
+                                if cell_ii not in same_possibles:
+                                    cell_ii.remove_possible_value(possibles[0])
+                                    cell_ii.remove_possible_value(possibles[1])
+                    # Naked Triples
+                    if len(same_possibles) == 3:
+                        if len(possibles) == 3:
+                            for cell_ii in box:
+                                if cell_ii not in same_possibles:
+                                    cell_ii.remove_possible_value(possibles[0])
+                                    cell_ii.remove_possible_value(possibles[1])
+                                    cell_ii.remove_possible_value(possibles[2])
+                    # print(len(same_possibles))
+                    # for c in same_possibles:
+                    #    print(f"{c} has possibles {c.possible_values}")
+        for box in self.boxes:
+            occurrences: list[list[Cell]] = []
+            for n in range(9):
+                a: list[Cell] = []
+                occurrences.append(a)
+                for cell in box:
+                    if (n + 1) in cell.possible_values:
+                        a.append(cell)
+            print(f"{"_" * 55}\nNew box output")
+            for n in range(9):
+                print(f"Cells where {n+1} is possible: {occurrences[n]}")
+            for occurrence in occurrences:
+                if len(occurrence) == 2:
+                    if occurrence[0].parent_column == occurrence[1].parent_column:
+                        print("Hidden Pair found!")
+                        for cell in occurrence[0].parent_column:
+                            if cell not in occurrence:
+                                print(occurrence[0].possible_values)
+                                try:
+                                    cell.remove_possible_value(occurrence[0].possible_values[0])
+                                    cell.remove_possible_value(occurrence[0].possible_values[1])
+                                except IndexError:
+                                    pass
+            # print(occurrences)
+
+            '''
+            if 2 <= occurrence_amount <= 3:
+                if occurrence[0].parent_row == occurrence[1].parent_row == occurrence[-1].parent_row:
+                    for cell in occurrence[0].parent_column:
+                        # if cell != occurrence[0]:
+                        if cell not in occurrence:
+                            cell.remove_possible_value(i)
+                if occurrence[0].parent_column == occurrence[1].parent_column == occurrence[-1].parent_column:
+                    for cell in occurrence[0].parent_row:
+                        # if cell != occurrence[0]:
+                        if cell not in occurrence:
+                            cell.remove_possible_value(i)
+        '''
+        print("end")
+
+
+class Cell:
+
+    def __init__(self, value: int = 0) -> None:
+        self.value: int = None
+        self.possible_values: list[int] = None
+        self.assign_value(value)
+        self.parent_box: list[Cell] = None
+        self.parent_row: list[Cell] = None
+        self.parent_column: list[Cell] = None
+        # self.parents: list[list[Cell]] = None   # Boxes, Rows, Columns
+        self.parent_grid: Grid = None
+
+    def __str__(self) -> str:
         return str(self.value)
 
-    def __repr__(self):
-        return str(self.value)
+    def __repr__(self) -> str:
+        return f"Cell({self.value}: {self.possible_values})"
 
-    def __add__(self,other):
-        return self.value+other
+    def __add__(self, other) -> int:
+        return self.value + other
 
-    def __radd__(self,other):
+    def __radd__(self, other) -> int:
         return self.__add__(other)
 
-    def getValue(self):
-        return self.value
+    def remove_possible_value(self, value: int) -> None:
+        try:
+            self.possible_values.remove(value)
+        except ValueError:
+            pass
+        if len(self.possible_values) == 1 and self.value == 0:
+            self.assign_value(self.possible_values[0])
+            self.possible_values = []
 
-    #sets the value of the cell to the one provided in the parameters
-    #will then go into the parents list and remove it's value from the
-    #list of possibles from all cells sharing its box, row, and column
-    def setValue(self,v):
-        old_v = self.value
-        self.value = v
-        if self.value!=old_v: printSdk(grid)
-        if v != 0: self.PossibleVals = []
-        for p in self.Parents:
+    def assign_value(self, value: int) -> None:
+        if type(value) is not int:
+            raise TypeError("Invalid type for Sudoku Puzzle: {type(value)}. Puzzle should consist of type 'int'")
+        if not (0 <= value <= 9):
+            raise ValueError("Invalid integer: numbers should be from 0 to 9.")
+        if value == self.value:
+            pass
+        else:
+            self.value = value
+            if value == 0:
+                self.possible_values = [i + 1 for i in range(9)]
+            else:
+                self.possible_values = []
             try:
-                for c in p:
-                    c.removePossible(v)
+                if self.parent_grid.printable:
+                    print(self.parent_grid)
+                parents = [self.parent_box, self.parent_row, self.parent_column]
+                for parent in parents:
+                    for cell in parent:
+                        cell.remove_possible_value(value)
             except AttributeError:
                 pass
 
-    def getPosVals(self):
-        return self.PossibleVals
-
-    #goes through the list of possible values and pops the one desired
-    #if the list contains only one entry the list will pop that entry and
-    #update the cell's value
-    def removePossible(self,x):
-        try:
-            pos = self.PossibleVals.index(x)
-            self.PossibleVals.pop(pos)
-        except ValueError:
-            pass
-        if len(self.PossibleVals)==1 and self.value == 0:
-            self.setValue(self.PossibleVals.pop(0))
-            #self.PossibleVals = []
-
-    #for some reason it was necessary to implement a remove that doesn't
-    #update the value
-    #might merge with faction above as to not repeat code
-    def tempRemove(self,x):
-        try:
-            pos = self.PossibleVals.index(x)
-            self.PossibleVals.pop(pos)
-        except ValueError: pass
+    def reset(self) -> None:
+        self.value = 0
+        self.possible_values = [i+1 for i in range(9)]
 
 
+puzzle = [0, 0, 0, 0, 0, 0, 0, 0, 0,
+          9, 0, 0, 0, 6, 3, 0, 0, 1,
+          0, 0, 6, 0, 2, 7, 9, 5, 0,
+          1, 0, 0, 0, 0, 9, 0, 0, 0,
+          4, 7, 3, 0, 0, 0, 1, 6, 9,
+          0, 0, 0, 1, 0, 0, 0, 0, 8,
+          0, 8, 4, 3, 1, 0, 6, 0, 0,
+          2, 0, 0, 6, 8, 0, 0, 0, 5,
+          0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-#grid, boxes, rows, and columns are currently stored as a list
-#should update into a data structure for better manipulation and
-#grid compatibility
-grid = []
-boxes = []
-rows = []
-columns = []
 
-SudokuBook = [
-	[0,0,0,0,6,7,0,0,0,   #diab
-	 0,1,0,9,0,0,7,0,0,
-	 0,0,0,0,0,3,9,4,0,
-	 1,0,0,0,4,0,3,0,7,
-	 0,6,0,0,0,0,0,8,0,
-	 4,0,5,0,9,0,0,0,2,
-	 0,5,3,2,0,0,0,0,0,
-	 0,0,6,0,0,5,0,2,0,
-	 0,0,0,8,3,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0,   #hard
-	 9,0,0,0,6,3,0,0,1,
-	 0,0,6,0,2,7,9,5,0,
-	 1,0,0,0,0,9,0,0,0,
-	 4,7,3,0,0,0,1,6,9,
-	 0,0,0,1,0,0,0,0,8,
-	 0,8,4,3,1,0,6,0,0,
-	 2,0,0,6,8,0,0,0,5,
-	 0,0,0,0,0,0,0,0,0],
-	[0,0,0,4,0,0,0,0,9,   #hard
-	 9,0,3,0,2,0,4,0,0,
-	 4,2,0,0,9,3,6,0,0,
-	 6,0,0,0,0,0,5,0,0,
-	 0,4,0,0,0,0,0,9,0,
-	 0,0,7,0,0,0,0,0,3,
-	 0,0,9,1,6,0,0,5,4,
-	 0,0,4,0,3,0,8,0,1,
-	 2,0,0,0,0,7,0,0,0],
-	[0,7,0,0,0,8,0,0,0,   #diab
-	 0,0,4,0,0,0,2,5,6,
-	 0,6,0,0,0,2,0,0,7,
-	 0,0,3,0,7,0,0,6,0,
-	 0,0,0,8,0,3,0,0,0,
-	 0,9,0,0,6,0,4,0,0,
-	 2,0,0,3,0,0,0,9,0,
-	 3,5,9,0,0,0,6,0,0,
-	 0,0,0,7,0,0,0,1,0],
-	[3,0,0,2,0,7,0,0,0,   #hard
-	 4,0,0,0,0,1,0,0,0,
-	 0,0,7,8,0,0,0,9,0,
-	 5,8,0,6,0,0,0,0,4,
-	 2,0,0,0,9,0,0,0,6,
-	 6,0,0,0,0,4,0,1,9,
-	 0,5,0,0,0,9,2,0,0,
-	 0,0,0,3,0,5,0,0,8],
-	[7,0,4,0,0,0,0,0,0,   #hard
-	 0,0,6,0,8,5,0,4,0,
-	 8,0,1,0,2,6,0,0,0,
-	 0,0,7,8,0,0,0,0,4,
-	 1,0,0,0,0,0,0,0,7,
-	 2,0,0,0,0,7,3,0,0,
-	 0,0,0,6,1,0,5,0,3,
-	 0,8,0,3,5,0,1,0,0,
-	 0,0,0,0,0,0,4,0,2],
-	[0,0,0,1,0,0,7,0,2,
-	 0,5,2,0,0,8,0,0,0,
-	 0,0,3,4,2,0,0,0,0,
-	 0,0,8,0,0,0,9,5,0,
-	 9,0,0,0,0,0,0,0,6,
-	 0,2,7,0,0,0,8,0,0,
-	 0,0,0,0,3,4,5,0,0,
-	 0,0,0,6,0,0,1,7,0,
-	 6,0,4,0,0,9,0,0,0],
-	[5,3,0,0,7,0,0,0,0,   #idk
-	 6,0,0,1,9,5,0,0,0,
-	 0,9,8,0,0,0,0,6,0,
-	 8,0,0,0,6,0,0,0,3,
-	 4,0,0,8,0,3,0,0,1,
-	 7,0,0,0,2,0,0,0,6,
-	 0,6,0,0,0,0,2,8,0,
-	 0,0,0,4,1,9,0,0,5,
-	 0,0,0,0,8,0,0,7,9]]
+def create_grid(puzzle: list[int]) -> Grid:
+    return Grid(puzzle)
 
-#make test grid
-def simpleSdkPop():
-    for i in range(9):
-        for j in range(9):
-            grid[i].append(j+1)
 
-#turn list from puzzle book into a grid
-def generatePuzzle(grid,puzzle):
-    for i in puzzle:
-        grid.append(cell(i))
-    populateBoxes(grid)
-    populateRows(grid)
-    populateColumns(grid)
+def main() -> Grid:
+    grid = Grid(puzzle)
+    grid.solve()
+    # grid = create_grid(puzzle)
+    # while not grid.is_solved():
+    #    grid.solve()
     return grid
 
-#creates boxes
-def populateBoxes(grid):
-    box = -1
-    for i, c in enumerate(grid):
-        if (i%3) == 0: box+=1
-        boxes[box].append(c)
-        c.Parents[0] = boxes[box]
-        if (i%9) == 8 and (i%27) != 26: box-=3
 
-#creates rows
-def populateRows(grid):
-    for i in range(9):
-        for j in range(0+9*i,9+9*i):
-            #print(j)
-            rows[i].append(grid[j])
-            grid[j].Parents[1] = rows[i]
+if __name__ == "__main__":
+    main()
 
-#creates columns
-def populateColumns(grid):
-    for i, c in enumerate(grid):
-        columns[i%9].append(c)
-        c.Parents[2] = columns[i%9]
-
-#prints grid as a puzzle
-def printSdk(grid):
-    print("___________________")
-    for i in range(9):
-        string = "|"
-        for j in range(0+9*i,9+9*i):
-            if grid[j].getValue() == 0: string += (" |")
-            else: string += (str(grid[j])+"|")
-        print(string)
-    print("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
-
-''' unused
-def purgePos(lst):
-    for i in lst:
-        val = i.value()
-        if val == 0: break
-'''
-#will solve a puzzle from the puzzle book
-def solve(puzzle):
-    clearGrid()
-    global grid
-    guessing = False
-    guesses = []
-    grid = generatePuzzle(grid,puzzle)
-    copy_grid = deepcopy(grid)
-    printSdk(grid)
-    while not solved():
-        updatePossibles()
-        for h, lst in enumerate([boxes,rows,columns]):
-            for l in lst:
-                tallies = []
-                for i in range(9):
-                    tallies.append([])
-                    for c in l:
-                        if (i+1) in c.PossibleVals:
-                            tallies[i].append(c)
-                for t in tallies:
-                    if len(t) == 1:
-                        t[0].setValue(tallies.index(t)+1)
-                if h==0:
-                    tallies = []
-                    for i in range(9):
-                        tallies.append([])
-                        for c in l:
-                            if (i+1) in c.PossibleVals:
-                                tallies[i].append(c)
-                    for i, t in enumerate(tallies):
-                        if 2 <= len(t) <= 3:
-                            for par in range(2):
-                                if t[0].Parents[par+1] == t[1].Parents[par+1] == t[-1].Parents[par+1]:
-                                    for c in t[0].Parents[par+1]:
-                                        c.tempRemove(i+1)
-                                    for c in t:
-                                        c.PossibleVals.append(i+1)
-        #this part attempts to see if the puzzle is stuck
-        if grid != copy_grid: copy_grid = copy(grid)
-        elif grid == copy_grid:
-            #if it is not guessing yet it sets a frame
-            if not guessing:
-                mgrid = deepcopy(grid)
-                guessing = True
-                #print("This puzzle is hard, let's try guessing")
-                for i in grid:
-                    if i.getValue() == 0:
-                        g = choice(i.PossibleVals)
-                        guesses.append(g)
-                        i.setValue(g)
-                        break
-            elif guessing:
-                #if solved(): break
-                for i, c in enumerate(grid):
-                    c.PossibleVals = [1,2,3,4,5,6,7,8,9]
-                    c.value = mgrid[i].getValue()
-                updatePossibles()
-                for i in grid:
-                    if i.getValue() == 0:
-                        for g in guesses:
-                            i.removePossible(g)
-                        if i.getValue() == 0:
-                            i.setValue(choice(i.PossibleVals))
-                        break
-                #print(grid)
-                #print(mgrid)
-
-#used to reinitialize grid's possible values due to laziness
-def updatePossibles():
-    for i in grid: i.setValue(i.getValue())
-
-#will determine whether or not the grid is solved
-#each box, row, and column needs a sum of 45 to return True
-def solved():
-    for area in (boxes,rows,columns):
-        for i in area:
-            total = 0
-            for j in i: total += j
-            if total != 45: return False
-    print("Solved!")
-    return True
-
-#intialize grid or empty and reinitialize
-def clearGrid():
-    global grid, boxes, rows, columns
-    grid = []
-    boxes = []
-    rows = []
-    columns = []
-    for i in [boxes,rows,columns]:
-        for j in range(9):
-            i.append([])
-
-#currently cannot solve diabolic / very hard
-#either impossible without guessing or requires logic i dont know
-solve(SudokuBook[-1])
-#printSdk()
+grid = main()
